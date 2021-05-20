cb306b1fd76f4b31cd4fa4a6d12796ebe489e1f2
Victor <81167569+Victor-Manoel-Public@users.noreply.github.com>
2021-05-19 22:13:07 -0300

    Update NumberOfDaysInMonth.java

--- exercises/section-5/NumberOfDaysInMonth.java
+++ exercises/section-5/NumberOfDaysInMonth.java
@@ -30,7 +30,7 @@ public class NumberOfDaysInMonth {
      */
 
     public static boolean isLeapYear(int year){
-        if( (year >=1) && (year <= 9999) ){
+        if( (year >= 1) && (year <= 9999) ){
             return (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
         }else{
             return false;
@@ -38,7 +38,7 @@ public class NumberOfDaysInMonth {
     }
     public static int getDaysInMonth(int month, int year){
         if ( (month < 1 || month > 12) || (year < 1 || year > 9999) ){
-            return - 1;
+            return -1;
         }
         switch (month){
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
