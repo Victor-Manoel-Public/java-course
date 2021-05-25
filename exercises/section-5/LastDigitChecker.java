71aca50eb9992b7e05ac370c1c05e73b2acdba24
Victor <81167569+Victor-Manoel-Public@users.noreply.github.com>
2021-05-25 16:02:54 -0300

    Create LastDigitChecker.java

---
+++ exercises/section-5/LastDigitChecker.java
@@ -0,0 +1,47 @@
+public class LastDigitChecker {
+    /*
+        Write a method named hasSameLastDigit with three parameters of type int.
+        Each number should be within the range of 10 (inclusive) - 1000 (inclusive).
+        If one of the numbers is not within the range, the method should return false.
+        The method should return true if at least two of the numbers share the same rightmost digit;
+        otherwise, it should return false.
+        EXAMPLE INPUT/OUTPUT:
+        * hasSameLastDigit (41, 22, 71); → should return true since 1 is the rightmost digit in numbers 41 and 71
+        * hasSameLastDigit (23, 32, 42); → should return true since 2 is the rightmost digit in numbers 32 and 42
+        * hasSameLastDigit (9, 99, 999); → should return false since 9 is not within the range of 10-1000
+
+
+        Write another method named isValid with one parameter of type int.
+        The method needs to return true if the number parameter is in range of 10(inclusive) - 1000(inclusive),
+        otherwise return false.
+        EXAMPLE INPUT/OUTPUT
+        * isValid(10); → should return true since 10 is within the range of 10-1000
+        * isValid(468); → should return true since 468 is within the range of 10-1000
+        * isValid(1051); → should return false since 1051 is not within the range of 10-1000
+
+ */
+    public static boolean hasSameLastDigit(int x, int y, int i){
+        if( (isValid(x)) && (isValid(y)) && (isValid(i)) ){
+            // x=y=i%=10; I tried this but somehow doesn't works
+            x%=10;
+            y%=10;
+            i%=10;
+            return ( (x == y) || (x == i) || (y == i));
+        }
+        return false;
+    }
+
+    public static boolean isValid(int x){
+        return ( (x > 9) && (x < 1001));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasSameLastDigit(41, 22, 71));
+        System.out.println(hasSameLastDigit(23, 32, 42));
+        System.out.println(hasSameLastDigit(9, 99, 999));
+        System.out.println(isValid(10));
+        System.out.println(isValid(468));
+        System.out.println(isValid(1051));
+    }
+
+}
