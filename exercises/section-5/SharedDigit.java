4f507d1ecbd07884e241a72fced41fadfef41467
Victor <81167569+Victor-Manoel-Public@users.noreply.github.com>
2021-06-07 17:42:20 -0300

    Update SharedDigit.java

--- exercises/section-5/SharedDigit.java
+++ exercises/section-5/SharedDigit.java
@@ -14,11 +14,8 @@ public class SharedDigit {
 
     */
     public static boolean hasSharedDigit(int x, int y){
-        if( (x > 99 || x < 10) || (y > 99 || y < 10) ) {
-            return false;
-        }else{
-            return( ( x/10 == y/10) || (x / 10 == y % 10) || ( y/10 == x % 10) || ( x%10 == y%10 ));
-        }
+        return (x <= 99 && x >= 10) && (y <= 99 && y >= 10) && ((x / 10 == y / 10) || (x / 10 == y % 10) ||
+               (y / 10 == x % 10) || (x % 10 == y % 10));
     }
 
     public static void main(String[] args) {
