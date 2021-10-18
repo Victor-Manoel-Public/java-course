e7e070114dae2dd717905ffb175eb5e3d15ca62f
Victor Manoel <victor.manoel.da.silva.28.10@gmail.com>
2021-10-18 10:57:24 -0300

    Section 8

---
+++ exercises/section-8/arrays/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] myArr = new int[10];
+        for (int i = 0; i < myArr.length ; i++) {
+            myArr[i] = i * 10;
+        }
+
+        printIntArray(myArr);
+    }
+
+    public static void printIntArray(int[] arr){
+        for(int i =0; i < arr.length; i++){
+            System.out.printf("#%d -> %d.%n", i, arr[i]);
+        }
+    }
+}
